7050a7cdcbc0885277445b99a62b444b6d8c2fa7
Atanas <a.arshinkov97@gmail.com>
2018-11-12 19:38:44 +0200

    Added FindElementByKey file

---
+++ FindElementByKey.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindElementByKey
+{
+    public class Program
+    {
+        private static readonly IDictionary _dictionary = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+
+            _dictionary.Add("text", "notepad");
+            _dictionary.Add("jpeg", "photoshop");
+            _dictionary.Add("mov", "sony-vegas");
+
+            FindEntryValueByKey("jpeg");
+
+        }
+
+        private static void FindEntryValueByKey(string key)
+        {
+            foreach (DictionaryEntry entry in _dictionary)
+            {
+                if (entry.Key.Equals(key))
+                {
+                    Console.WriteLine(entry.Value.ToString());
+                    return;
+                }
+            }
+
+            Console.WriteLine("No element with \"{0}\" key was found.", key);
+        }
+    }
+}
